f1fac45f848ea0e311b319171366f69517907622
kazi331 <kazisharifulislam52@gmail.com>
2024-05-05 14:49:36 +0600

    refactor: Update form and button selectors in index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,20 +1,22 @@
-const subsForm = document.querySelector('#subs-form');
-const newsLetterBtn = document.querySelector('#submit-btn');
+const form = document.querySelector('form');
+const btn = document.querySelector('#submit-btn');
 const message = document.querySelector('#message');
+const fieldset = document.querySelector("fieldset");
+
 let isLoading = false;
 
 const disableBtn = () => {
     isLoading = true;
-    newsLetterBtn.innerHTML = "Loading...";
-    newsLetterBtn.disabled = true;
+    btn.innerHTML = "Loading...";
+    btn.disabled = true;
 }
 const resetBtn = () => {
     isLoading = false;
-    newsLetterBtn.innerHTML = "Subscribe";
-    newsLetterBtn.disabled = false;
+    btn.innerHTML = "Subscribe";
+    btn.disabled = false;
 }
 
-subsForm.addEventListener("submit", async e => {
+form.addEventListener("submit", async e => {
     e.preventDefault();
     if (e.target.email.value === '') {
         e.target.email.style.outline = '1px solid red';
@@ -32,27 +34,31 @@ subsForm.addEventListener("submit", async e => {
         isLoading = true;
         e.target.email.style.outline = '1px solid gray';
         disableBtn();
+        try {
+            const res = await fetch("https://apis-dev.approveage.com/client/subscribe-to-news-letter", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email: e.target.email.value })
+            });
+            const data = await res.json();
+            if (data.status === 200) {
+                resetBtn();
+                message.innerText = "You have successfully subscribed to our newsletter";
+                form.reset();
+                fieldset.disabled = true;
+            }
 
-        const res = await fetch("https://apis-dev.approveage.com/client/subscribe-to-news-letter", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email: e.target.email.value })
-        });
-        const data = await res.json();
-        if (data.status === 200) {
-            // e.target.email.value = '';
-            console.log(data.message)
+        } catch (err) {
+            message.innerText = "An error occured, please try again later";
+            message.style.color = 'red';
             resetBtn();
-            message.innerHTML = "You have successfully subscribed to our newsletter";
-            subsForm.reset();
-            subsForm.disable = true;
-        }
 
+        }
     }
 
 
 })
 
-const fieldset = document.querySelector("#subs-form-field");
+
