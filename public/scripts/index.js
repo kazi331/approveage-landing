6b8f1e422f35850c793de537f15ff198db3616be
kazi331 <kazisharifulislam52@gmail.com>
2024-05-05 13:21:41 +0600

    subscribe to newsletter

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,58 @@
 const subsForm = document.querySelector('#subs-form');
-subsForm.addEventListener("submit", e => {
+const newsLetterBtn = document.querySelector('#submit-btn');
+const message = document.querySelector('#message');
+let isLoading = false;
+
+const disableBtn = () => {
+    isLoading = true;
+    newsLetterBtn.innerHTML = "Loading...";
+    newsLetterBtn.disabled = true;
+}
+const resetBtn = () => {
+    isLoading = false;
+    newsLetterBtn.innerHTML = "Subscribe";
+    newsLetterBtn.disabled = false;
+}
+
+subsForm.addEventListener("submit", async e => {
     e.preventDefault();
-    console.log(e.target.email.value);
+    if (e.target.email.value === '') {
+        e.target.email.style.outline = '1px solid red';
+        e.target.email.animate([
+            { transform: 'translateX(0px)' },
+            { transform: 'translateX(10px)' },
+            { transform: 'translateX(-10px)' },
+            { transform: 'translateX(0px)' }
+        ], {
+            duration: 100,
+            iterations: 2
+        });
+
+    } else {
+        isLoading = true;
+        e.target.email.style.outline = '1px solid gray';
+        disableBtn();
+
+        const res = await fetch("https://apis-dev.approveage.com/client/subscribe-to-news-letter", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: e.target.email.value })
+        });
+        const data = await res.json();
+        if (data.status === 200) {
+            // e.target.email.value = '';
+            console.log(data.message)
+            resetBtn();
+            message.innerHTML = "You have successfully subscribed to our newsletter";
+            subsForm.reset();
+            subsForm.disable = true;
+        }
+
+    }
+
+
 })
+
+const fieldset = document.querySelector("#subs-form-field");
